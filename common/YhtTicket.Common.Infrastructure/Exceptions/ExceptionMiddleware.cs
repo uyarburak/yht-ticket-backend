98c72227cd360d8ffbb8adf46c5bae4edd9e8893
uyarburak <burakuyar25@gmail.com>
2021-12-06 20:58:49 +0300

    FP-28 Generic error handling middleware implemented

---
+++ common/YhtTicket.Common.Infrastructure/Exceptions/ExceptionMiddleware.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Net;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace YhtTicket.Common.Infrastructure.Exceptions
+{
+    public class ExceptionMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public ExceptionMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (ApiException e)
+            {
+                var response = context.Response;
+                response.ContentType = "application/json";
+                response.StatusCode = (int)e.HttpStatusCode;
+
+                var result = JsonSerializer.Serialize(new { message = e.Message, code = e.ErrorCode });
+                await response.WriteAsync(result);
+            }
+            catch (Exception)
+            {
+                var response = context.Response;
+                response.ContentType = "application/json";
+                response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                
+                var result = JsonSerializer.Serialize(new { message = "Beklenmeyen hata", code = "INTERNAL_ERROR" });
+                await response.WriteAsync(result);
+            }
+        }
+    }
+}
---
+++ common/YhtTicket.Common.Infrastructure/Exceptions/ValidationException.cs
@@ -0,0 +1,10 @@
+﻿namespace YhtTicket.Common.Infrastructure.Exceptions
+{
+    public class ValidationException : ApiException
+    {
+        public ValidationException(string errorCode, string message) : base(System.Net.HttpStatusCode.BadRequest, errorCode, message)
+        {
+
+        }
+    }
+}
